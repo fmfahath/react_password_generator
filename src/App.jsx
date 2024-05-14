ce894816418ec991583f5f73ae58b7141bb071cd
fmfahath <fmfahath.dev@gmail.com>
2024-05-15 02:02:30 +0530

    error handler function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import logo from './assets/passwordLogo.png';
 function App() {
@@ -9,32 +9,47 @@ function App() {
   const [numbers, setNumbers] = useState(false);
   const [symbols, setSymbols] = useState(false);
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(false);
-  const [copyMessage, setCopyMessage] = useState(false)
+  const [errorState, setErrorState] = useState({
+    inputError: false,
+    checkListError: false,
+  });
+  const [copyMessage, setCopyMessage] = useState(false);
+
 
   const generatePassword = () => {
-
     let charSet = "";
     let generatedPassword = "";
 
-    if (!length.length) {
-      setErrorMessage(true);
+    if (!length) {
+      setErrorState({ ...errorState, inputError: true });
+      // console.log("inputError: ", errorState);
+      return;
     }
 
-    if (uppercase) charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (lowercase) charSet += "abcdefghijklmnopqrstuvwxyz";
-    if (numbers) charSet += "0123456789";
-    if (symbols) charSet += "`~!@#$%^&*()_+`";
+    if (uppercase || lowercase || numbers || symbols) {
+      if (uppercase) charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      if (lowercase) charSet += "abcdefghijklmnopqrstuvwxyz";
+      if (numbers) charSet += "0123456789";
+      if (symbols) charSet += "`~!@#$%^&*()_+`";
 
-    // console.log(charSet); 
-    for (let i = 0; i < length; i++) {
-      let randomIndex = Math.floor(Math.random() * charSet.length);
-      generatedPassword += charSet[randomIndex];
+      // console.log(charSet); 
+      for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * charSet.length);
+        generatedPassword += charSet[randomIndex];
+      }
+
+      setPassword(generatedPassword);
+
+    } else {
+      setErrorState({ ...errorState, checkListError: true });
+      // console.log("ChekError: ", errorState);
+      return;
     }
-
-    setPassword(generatedPassword);
-    setErrorMessage(false);
   }
+
+
+
+
 
   const copyPassword = () => {
     if (password) {
@@ -47,6 +62,33 @@ function App() {
   }
 
 
+  const handleInput = (e) => {
+    if (e.target.name === "input") {
+      parseInt(setLength(e.target.value));
+      setErrorState({ ...errorState, inputError: false });
+
+    }
+    else if (e.target.name === "uppercase") {
+      setUppercase(e.target.checked)
+      setErrorState({ ...errorState, checkListError: false });
+
+    }
+    else if (e.target.name === "lowecase") {
+      setLowercase(e.target.checked)
+      setErrorState({ ...errorState, checkListError: false });
+
+    }
+    else if (e.target.name === "numbers") {
+      setNumbers(e.target.checked)
+      setErrorState({ ...errorState, checkListError: false });
+    }
+    else {
+      setSymbols(e.target.checked)
+      setErrorState({ ...errorState, checkListError: false });
+    }
+  }
+
+
   return (
     <>
       <div className="container">
@@ -55,24 +97,24 @@ function App() {
           <h2>Password Generator</h2>
         </div>
         <div className="input">
-          <label htmlFor="input">Password Length: </label>
-          <input type="number" id='input' name='input' onChange={(e) => parseInt(setLength(e.target.value))} value={length} />
+          <label htmlFor="input" className={`${errorState.inputError ? "error" : ""}`}>Password Length: </label>
+          <input type="number" id='input' className={`${errorState.inputError ? "error" : ""}`} onChange={handleInput} name='input' value={length} />
         </div>
-        <div className="checkList">
+        <div className={`checkList ${errorState.checkListError ? "error" : ""}`}>
           <div className="chekList-item">
-            <input type="checkbox" id='uppercase' onChange={(e) => setUppercase(e.target.checked)} checked={uppercase} />
+            <input type="checkbox" id='uppercase' name='uppercase' onChange={handleInput} checked={uppercase} />
             <label htmlFor="uppercase">Include Uppercase</label>
           </div>
           <div className="chekList-item">
-            <input type="checkbox" id='lowecase' onChange={(e) => setLowercase(e.target.checked)} checked={lowercase} />
+            <input type="checkbox" id='lowecase' name='lowecase' onChange={handleInput} checked={lowercase} />
             <label htmlFor="lowecase">Include Lowercase</label>
           </div>
           <div className="chekList-item">
-            <input type="checkbox" id='numbers' onChange={(e) => setNumbers(e.target.checked)} checked={numbers} />
+            <input type="checkbox" id='numbers' name='numbers' onChange={handleInput} checked={numbers} />
             <label htmlFor="numbers">Include Numbes</label>
           </div>
           <div className="chekList-item">
-            <input type="checkbox" id='symbols' onChange={(e) => setSymbols(e.target.checked)} checked={symbols} />
+            <input type="checkbox" id='symbols' name='symbols' onChange={handleInput} checked={symbols} />
             <label htmlFor="symbols">Include Symbols</label>
           </div>
         </div>
