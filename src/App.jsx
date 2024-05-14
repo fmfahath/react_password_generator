489b2ea08bd1d592045a0b3119d1eae93902b828
fmfahath <fmfahath.dev@gmail.com>
2024-05-14 17:13:21 +0530

    UI created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import { useState } from 'react'
 import './App.css'
-
+import logo from './assets/passwordLogo.png';
 function App() {
 
   return (
     <>
-      <p>test</p>
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="" />
+          <h2>Password Generator</h2>
+        </div>
+        <div className="input">
+          <label htmlFor="input">Password Length: </label>
+          <input type="number" id='input' name='input' />
+        </div>
+        <div className="checkList">
+          <div className="chekList-item">
+            <input type="checkbox" id='uppercase' />
+            <label htmlFor="uppercase">Include Uppercase</label>
+          </div>
+          <div className="chekList-item">
+            <input type="checkbox" id='lowecase' />
+            <label htmlFor="lowecase">Include Lowercase</label>
+          </div>
+          <div className="chekList-item">
+            <input type="checkbox" id='numbers' />
+            <label htmlFor="numbers">Include Numbes</label>
+          </div>
+          <div className="chekList-item">
+            <input type="checkbox" id='symbols' />
+            <label htmlFor="symbols">Include Symbols</label>
+          </div>
+        </div>
+        <div className='generate-btn'>
+          <button>Generate</button>
+        </div>
+        <div className="password">
+          <input type="text" readOnly />
+          <button>Copy</button>
+        </div>
+
+      </div>
     </>
   )
 }
