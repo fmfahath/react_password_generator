837e3ee71c635cdb87ddd80d14a23174e6fd6df9
fmfahath <fmfahath.dev@gmail.com>
2024-05-14 18:06:12 +0530

    getting user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 import logo from './assets/passwordLogo.png';
 function App() {
+
+  const [length, setLength] = useState("8");
+  const [uppercase, setUppercase] = useState(false);
+  const [lowercase, setLowercase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const generatePassword = () => {
+    console.log(length, uppercase, lowercase, numbers, symbols);
+  }
 
   return (
     <>
@@ -11,31 +23,31 @@ function App() {
         </div>
         <div className="input">
           <label htmlFor="input">Password Length: </label>
-          <input type="number" id='input' name='input' />
+          <input type="number" id='input' name='input' onChange={(e) => parseInt(setLength(e.target.value))} value={length} />
         </div>
         <div className="checkList">
           <div className="chekList-item">
-            <input type="checkbox" id='uppercase' />
+            <input type="checkbox" id='uppercase' onChange={(e) => setUppercase(e.target.checked)} checked={uppercase} />
             <label htmlFor="uppercase">Include Uppercase</label>
           </div>
           <div className="chekList-item">
-            <input type="checkbox" id='lowecase' />
+            <input type="checkbox" id='lowecase' onChange={(e) => setLowercase(e.target.checked)} checked={lowercase} />
             <label htmlFor="lowecase">Include Lowercase</label>
           </div>
           <div className="chekList-item">
-            <input type="checkbox" id='numbers' />
+            <input type="checkbox" id='numbers' onChange={(e) => setNumbers(e.target.checked)} checked={numbers} />
             <label htmlFor="numbers">Include Numbes</label>
           </div>
           <div className="chekList-item">
-            <input type="checkbox" id='symbols' />
+            <input type="checkbox" id='symbols' onChange={(e) => setSymbols(e.target.checked)} checked={symbols} />
             <label htmlFor="symbols">Include Symbols</label>
           </div>
         </div>
         <div className='generate-btn'>
-          <button>Generate</button>
+          <button onClick={generatePassword}>Generate</button>
         </div>
         <div className="password">
-          <input type="text" readOnly />
+          <input type="text" readOnly value={password} />
           <button>Copy</button>
         </div>
 
